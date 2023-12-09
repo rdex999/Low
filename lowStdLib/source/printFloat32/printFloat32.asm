056ac2d76196611410b65cc175902bd6caf23b37
rdex999 <didabom2150@gmail.com>
2023-12-09 11:15:06 +0200

    Made printFloat32 support the stack update

--- lowStdLib/source/printFloat32/printFloat32.asm
+++ lowStdLib/source/printFloat32/printFloat32.asm
@@ -11,14 +11,16 @@ section .text
 ; params:
 ; 1) {float} the float to print ( xmm0 )
 ; 2) {int} the amount of digits to print after the point ( rdi )
-; 3) {byte*} the location on the stack (given by the compiler) ( rsi )
 printFloat32:
-    cvtss2si eax, xmm0 ; convert to int 13.1234 -> 13
-    mov rbx, rdi
+    push rbp
+    mov rbp, rsp
+    sub rsp, 20 
 
-    mov edi, eax 
-    shr edi, 31
-    jz printFloat32Init
+    lea rsi, [rbp - 1]
+    mov ecx, edi ; store second argument in ecx
+    cvtss2si eax, xmm0 ; convert xmm0 to int in eax
+    shr eax, 31
+    jz printFloat32_posInit ; jump if eax is positivea
 
     mov BYTE [rsi], '-'
     mov rax, 1
@@ -26,50 +28,49 @@ printFloat32:
     mov rdx, 1
     syscall
 
-    movss xmm1, DWORD [mOneF32]
+    movss xmm1, DWORD [mOneF32] ; multiply by -1
     mulss xmm0, xmm1
-    cvtss2si eax, xmm0 ; convert to int 13.1234 -> 13
+    movss xmm2, xmm0
 
-printFloat32Init: 
-    add rsi, 10 ; alloc 10 bytes 
-    mov rcx, 1
-    mov BYTE [rsi+1], '.'
+    cvtss2si eax, xmm2  ; floor xmm2
+    cvtsi2ss xmm2, eax  ;
 
-.printIntLoop:
-    xor rdx, rdx
-    mov edi, 10
-    div edi
-    add dl, 48
-    mov [rsi], dl
-    dec rsi
-    inc rcx
-    cmp eax, 0
-    jne .printIntLoop
+    movss xmm1, xmm0
+    subss xmm1, xmm2    ; get the fractional part in xmm1 (13.123 -> 0.123)
 
-    mov rax, 1
-    mov rdi, 1
-    inc rsi
-    mov rdx, rcx
-    syscall
+    movss xmm2, DWORD [tenF32]
+    jmp printFloat32_fracLoop
 
-    cvtss2si edi, xmm0 ; convert to int 13.1234 -> 13
-    cvtsi2ss xmm1, edi ; convert to float32 13 -> 13.0
-    subss xmm0, xmm1   ; gen the fractional part 13.1234 - 13 = 0.1234
+printFloat32_posInit:
+    movss xmm2, xmm0
+    cvtss2si eax, xmm2  ; floor xmm2
+    cvtsi2ss xmm2, eax  ; 
+    movss xmm1, xmm0
+    subss xmm1, xmm2
+    movss xmm2, DWORD [tenF32]
 
-    mov rcx, 0
-    movss xmm1, DWORD [tenF32]
-.mulTenLoop: ;          multiply by 10.0 to shift the fractional part 0.1234 -> 1.234 -> 12.34 -> .....
-    mulss xmm0, xmm1
-    inc rcx
-    cmp rcx, rbx
-    jne .mulTenLoop
+printFloat32_fracLoop:
+    mulss xmm1, xmm2
+    dec ecx
+    jnz printFloat32_fracLoop
 
-    cvtss2si eax, xmm0 ; convert to int 123.4 -> 123
-
-    add rsi, 10 ; alloc 10 bytes 
+    cvtss2si eax, xmm1
     xor rcx, rcx
+printFloat32_fracPrintLoop:
+    xor rdx, rdx
+    mov edi, 10
+    div edi
+    add dl, 48
+    mov [rsi], dl
+    inc rcx
+    dec rsi
+    test eax, eax
+    jnz printFloat32_fracPrintLoop
 
-.printIntLoop2:
+    mov BYTE [rsi], '.'
+    dec rsi
+    cvtss2si eax, xmm0
+printFloat32_intLoop:
     xor rdx, rdx
     mov edi, 10
     div edi
@@ -77,12 +78,16 @@ printFloat32Init:
     mov [rsi], dl
     dec rsi
     inc rcx
-    cmp rax, 0
-    jne .printIntLoop2
+    test eax, eax
+    jnz printFloat32_intLoop
 
     mov rax, 1
     mov rdi, 1
     inc rsi
     mov rdx, rcx
+    inc rdx
     syscall
+
+    mov rsp, rbp
+    pop rbp
     ret
